52d37383492a8fb1982e0504724e2238e365de63
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2024-06-16 05:40:32 -0400

    Better UI with `sweetalert2`

--- frontend/src/components/LoginComponent.jsx
+++ frontend/src/components/LoginComponent.jsx
@@ -2,15 +2,18 @@ import PropTypes from "prop-types";
 import { useCallback, useEffect } from "react";
 import "./LoginComponent.css";
 import axios from "axios";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = ({ className = "" }) => {
+  const navigate = useNavigate();
+
   const onButtonBackgroundClick = useCallback(() => {
     // Check if the user exists
     const username = document.querySelector(".username-field").value;
     const password = document.querySelector(".password-field").value;
-
     if (!username || !password) {
-      alert("Please enter a username and password");
+      Swal.fire("Error", "Please enter a username and password", "error");
       return;
     }
 
@@ -25,38 +28,44 @@ const LoginForm = ({ className = "" }) => {
         if (response.status === 200) {
           localStorage.setItem("u_name", username);
           localStorage.setItem("uid", response.data.user_id);
-          window.location.href = "/";
+          Swal.fire("Logged in!", "You are now logged in.", "success").then(
+            () => {
+              navigate("/");
+            }
+          );
         } else {
           console.error("response", response);
         }
       })
-      .catch((error) => {
+      .catch(async (error) => {
         console.error("error", error);
         const response = error.response;
+
+        // User not found, register
         if (response.status === 404) {
-          // User not found, register
-          axios
-            .post("user/register", {
-              name: username,
-              password: password,
-            })
-            .then((response) => {
-              console.info("response", response);
-              if (response.status === 200) {
-                // Register successful
-                localStorage.setItem("u_name", username);
-                localStorage.setItem("uid", response.data.user_id);
-                window.location.href = "/";
-              } else {
-                console.error("response", response);
-              }
-            })
-            .catch((error) => {
-              alert(error.response.data.error);
-              console.error("error", error);
-            });
+          Swal.fire({
+            title: "User not found",
+            text: "Do you want to register?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, register",
+            cancelButtonText: "No, cancel",
+          }).then(async (result) => {
+            if (result.isConfirmed) {
+              await registerUser(username, password);
+              Swal.fire(
+                "Registered!",
+                "You are now registered and logged in.",
+                "success"
+              ).then(() => {
+                navigate("/");
+              });
+            } else if (result.dismiss === Swal.DismissReason.cancel) {
+              Swal.fire("Cancelled", "You are not registered", "error");
+            }
+          });
         } else if (response.status === 401) {
-          alert(error.response.data.error);
+          Swal.fire("Error", response.data.error, "error");
         }
       });
   }, []);
@@ -84,3 +93,24 @@ LoginForm.propTypes = {
 };
 
 export default LoginForm;
+async function registerUser(username, password) {
+  axios
+    .post("user/register", {
+      name: username,
+      password: password,
+    })
+    .then((response) => {
+      console.info("response", response);
+      if (response.status === 200) {
+        // Register successful
+        localStorage.setItem("u_name", username);
+        localStorage.setItem("uid", response.data.user_id);
+      } else {
+        console.error("response", response);
+      }
+    })
+    .catch((error) => {
+      alert(error.response.data.error);
+      console.error("error", error);
+    });
+}
