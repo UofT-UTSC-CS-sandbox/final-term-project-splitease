c16035dec158dfa5abe2a6c0eef10fb4ce1f765e
zhaohan77 <tommyhuang2017@gmail.com>
2024-07-29 14:43:36 -0400

    fix(frontend)
    remove the avatar feature in the  sharing detail component

--- frontend/src/components/SharingDetail.jsx
+++ frontend/src/components/SharingDetail.jsx
@@ -4,13 +4,13 @@ import { useNavigate } from "react-router-dom";
 import "./SharingDetail.css";
 
 const SharingDetail = ({ sharings }) => {
-  const navigate = useNavigate();
+
 
   return (
     <div className="activity_share">
       {sharings.map((sharing) => (
         <div key={sharing.id} className="activity-item">
-          <img src={sharing.avatar} className="avatar" />
+
           <div className="activity-details">
             <div className="activity-name">{sharing.name}</div>
             <div className="activity-type">{sharing.description}</div>
